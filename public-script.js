979127354e7cf42d76ef0c4c4e6a4e87fbaf2950
sorakoki <zpzezn@gmail.com>
2025-10-11 21:47:26 +0900

    Sync index.html from remote

---
+++ public-script.js
@@ -0,0 +1,18 @@
+fetch("properties.json")
+  .then(res => res.json())
+  .then(data => {
+    const list = document.getElementById("list");
+    data.forEach(p => {
+      const card = document.createElement("div");
+      card.className = "property-card";
+      card.innerHTML = `
+        <strong>物件名:</strong> ${p.name}<br>
+        <strong>価格:</strong> ${p.price} 円<br>
+        <strong>所在地:</strong> ${p.location}<br>
+        <strong>間取り:</strong> ${p.layout}<br>
+        <strong>説明:</strong> ${p.description}<br>
+        <img src="${p.image}" alt="物件画像">
+      `;
+      list.appendChild(card);
+    });
+  });
